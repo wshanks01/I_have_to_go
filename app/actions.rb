3bfc6aa85b6d514718ada4fa85882279c8a60d31
vagrant <vagrant@lighthouse-labs>
2016-06-15 20:48:12 +0000

    add post actions

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,17 @@
 # Homepage (Root path)
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+end
+
 get '/' do
   erb :index
+end
+
+post '/' do
+  address = params[:address]
+  current_user.address = address
+  current_user.save
+  redirect '/results'
 end
