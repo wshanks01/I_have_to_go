a1039503f2efe72ae33018462aff5f7b0c8f4839
vagrant <vagrant@lighthouse-labs>
2016-06-15 21:15:08 +0000

    added login/signup/boostrap

--- app/actions.rb
+++ app/actions.rb
@@ -9,9 +9,45 @@ get '/' do
   erb :index
 end
 
+get '/signup' do
+  @user = User.new
+  erb :'signup'
+end
+
+get '/login' do
+  erb :'login'
+end
+
 post '/' do
   address = params[:address]
   current_user.address = address
   current_user.save
   redirect '/results'
 end
+
+post '/signup' do
+  username = params[:username]
+  password = params[:password]
+  @user = User.new({username: username, password: password})  
+
+  if @user.save
+    redirect '/tracks'
+  else
+    erb :'/signup'
+  end
+end
+
+post '/login' do 
+  username = params[:username]
+  password = params[:password]
+  
+  user = User.find_by(username: username)
+
+  if user && user.password == password
+      session[:user_id] = user.id
+      redirect(to('/tracks'))
+  else
+      erb :'/login'
+  end
+end 
+
